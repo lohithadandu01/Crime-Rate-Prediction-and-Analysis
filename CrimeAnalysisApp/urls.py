6e84cb07b0969fa9ab19a146ef629cedf619ce30
lohithadandu01 <lohithadandu0110@gmail.com>
2026-05-02 22:42:49 +0530

    fix url name index

--- CrimeAnalysisApp/urls.py
+++ CrimeAnalysisApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
 
     path('adminlogin/', views.AdminLogin, name='adminlogin'),
     path('admin/', views.Admin, name='admin'),
